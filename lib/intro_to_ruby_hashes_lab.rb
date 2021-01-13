5e9e668f64fc0823a7c028bd100b558c319fdb17
Liz Burke <liz.burke.chi@gmail.com>
2021-01-13 21:43:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,14 @@ def my_hash
 end
 
 def pioneer
-
+  {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {
+    :id => "number"
+  }
 end
